45040a08bb4706e8ee16cc5ac625546c3b7e7772
lucianoarendt <luciano.arendt@unesp.br>
2026-02-24 02:38:20 -0300

    feat: word input

--- src/components/atoms/LetterBox.tsx
+++ src/components/atoms/LetterBox.tsx
@@ -1,13 +1,5 @@
 type Props = {
   letter: string
-<<<<<<< Updated upstream
-  maxSize: number
-}
-
-export function LetterBox({ letter, maxSize }: Props) {
-  return (
-    <div className={`aspect-square w-[${maxSize}%] md:w-12 border flex text-[#222222] items-center justify-center font-bold text-lg bg-gray-300`}>
-=======
   active?: boolean
 }
 
@@ -15,11 +7,10 @@ export function LetterBox({ letter, active }: Props) {
   return (
     <div
       className={`
-        w-12 h-12 border flex items-center text-[#222222] justify-center text-xl font-bold uppercase bg-gray-300
+        aspect-square md:w-12 border flex text-[#222222] items-center justify-center font-bold text-lg bg-gray-300
         ${active ? "border-black" : "border-gray-300"}
       `}
     >
->>>>>>> Stashed changes
       {letter}
     </div>
   )
