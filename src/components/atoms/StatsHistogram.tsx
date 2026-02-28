8032c941f1bc58527607d1c68e5a4d79c316200c
ViniciusCas <viniccasi@gmail.com>
2026-02-28 16:34:03 -0300

    fix histogram width inconsistence

--- src/components/atoms/StatsHistogram.tsx
+++ src/components/atoms/StatsHistogram.tsx
@@ -7,14 +7,20 @@ type StatsHistogramProps = {
 };
 
 export function StatsHistogram({label = "", max = 0, value = 0}: StatsHistogramProps) {
-  const barLength = max > 0 ? `w-[${Math.round(((value / max) * 100))}%]` : "w-fit";
-  const barColor =  value > 0 ? "bg-[var(--brand-green)]" : 'bg-[var(--brand-dark)]'
+  const barLength = value > 0 ? `${Math.round(((value / max) * 100))}%` : "fit-content";
+  const barColor  = value > 0 ? "bg-[var(--brand-green)]" : 'bg-[var(--brand-dark)]'
+  const textColor = value > 0 ? "text-black": "text-(--brand-cream)"
+
   return (
     <div className="flex text-[clamp(0.95rem,5vw,1.3rem)]">
       <p className="mr-2 w-[2ch]">{label}</p>
-      <div className={clsx(
-      `${barLength} ${barColor} px-3 text-[clamp(0.95rem,5vw,1.3rem)] rounded-lg text-end`
-      )}
+      <div className={
+        clsx(
+          `${barColor} ${textColor} px-3 text-[clamp(0.95rem,5vw,1.3rem)] rounded-lg text-end`
+        )}
+        style={{
+          width: barLength,
+        }}
       >
         {value}
       </div>
